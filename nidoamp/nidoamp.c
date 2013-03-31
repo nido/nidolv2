fa7be62620e9559ca5cb7828c294471ac633eb72
Nido Media <nido@foxserver.be>
2013-03-31 16:59:02 +0200

    indentation to k&r standards

--- nidoamp/nidoamp.c
+++ nidoamp/nidoamp.c
@@ -36,160 +36,157 @@
 
 
 typedef enum {
-	AMP_GAIN   = 0,
-	AMP_INPUT  = 1,
-	AMP_OUTPUT = 2
+    AMP_GAIN = 0,
+    AMP_INPUT = 1,
+    AMP_OUTPUT = 2
 } PortIndex;
 
 typedef struct {
-	float* gain;
-	float* input;
-	float* output;
-	fftw_complex* complex_buffer;
-	double* real_buffer;
-	float* buffer;
-	int buffer_index;
-	fftw_plan forward;
-	fftw_plan backward;
+    float *gain;
+    float *input;
+    float *output;
+    fftw_complex *complex_buffer;
+    double *real_buffer;
+    float *buffer;
+    int buffer_index;
+    fftw_plan forward;
+    fftw_plan backward;
 } Amp;
 
 LV2_Handle
-instantiate(const LV2_Descriptor*     descriptor,
-            double                    rate,
-            const char*               bundle_path,
-            const LV2_Feature* const* features)
+instantiate(const LV2_Descriptor * descriptor,
+	    double rate,
+	    const char *bundle_path, const LV2_Feature * const *features)
 {
-	Amp* amp = (Amp*)malloc(sizeof(Amp));
-	
+    Amp *amp = (Amp *) malloc(sizeof(Amp));
 
-	return (LV2_Handle)amp;
+
+    return (LV2_Handle) amp;
 }
 
-static void
-connect_port(LV2_Handle instance,
-             uint32_t   port,
-             void*      data)
+static void connect_port(LV2_Handle instance, uint32_t port, void *data)
 {
-	Amp* amp = (Amp*)instance;
+    Amp *amp = (Amp *) instance;
 
-	switch ((PortIndex)port) {
-	case AMP_GAIN:
-		amp->gain = (float*)data;
-		break;
-	case AMP_INPUT:
-		amp->input = (float*)data;
-		break;
-	case AMP_OUTPUT:
-		amp->output = (float*)data;
-		break;
+    switch ((PortIndex) port) {
+    case AMP_GAIN:
+	amp->gain = (float *) data;
+	break;
+    case AMP_INPUT:
+	amp->input = (float *) data;
+	break;
+    case AMP_OUTPUT:
+	amp->output = (float *) data;
+	break;
+    }
+}
+
+void activate(LV2_Handle instance)
+{
+    Amp *amp = (Amp *) instance;
+    amp->gain = malloc(sizeof(float));
+    amp->complex_buffer =
+	malloc(sizeof(fftw_complex) * (FOURIER_SIZE / 2 + 1));
+    amp->real_buffer = malloc(sizeof(double) * (FOURIER_SIZE));
+    amp->buffer = malloc(sizeof(float) * BUFFER_SIZE);
+    amp->buffer_index = 0;
+    amp->forward =
+	fftw_plan_dft_r2c_1d(FOURIER_SIZE, amp->real_buffer,
+			     amp->complex_buffer, FFTW_ESTIMATE);
+    amp->backward =
+	fftw_plan_dft_c2r_1d(FOURIER_SIZE, amp->complex_buffer,
+			     amp->real_buffer, FFTW_ESTIMATE);
+}
+
+static void fftprocess(Amp * amp, float *buffer)
+{
+    int iterator;
+    double *real_buffer = amp->real_buffer;
+
+    static float ponies = -1.0;
+    for (iterator = 0; iterator < FOURIER_SIZE; iterator++) {
+	real_buffer[iterator] = buffer[iterator];
+    }
+    fftw_execute(amp->forward);
+    fftw_execute(amp->backward);
+    for (iterator = 0; iterator < FOURIER_SIZE; iterator++) {
+	buffer[iterator] = (float) (real_buffer[iterator] / FOURIER_SIZE);
+    }
+}
+
+void run(LV2_Handle instance, uint32_t n_samples)
+{
+    Amp *amp = (Amp *) instance;
+    const float *const input = amp->input;
+    float *const output = amp->output;
+    float *buffer = amp->buffer;
+    uint32_t readindex = 0;
+    uint32_t readcount;
+
+    do {
+	uint32_t iterator;
+	uint32_t bufferlength =
+	    (uint32_t) (FOURIER_SIZE - (amp->buffer_index % FOURIER_SIZE));
+	readcount = n_samples;
+	if (amp->buffer_index + readcount > bufferlength) {
+	    readcount = bufferlength;
 	}
-}
 
-void
-activate(LV2_Handle instance)
-{
-	Amp* amp = (Amp*)instance;
-	amp->gain = malloc(sizeof(float));
-	amp->complex_buffer = malloc(sizeof(fftw_complex) * (FOURIER_SIZE/2+1));
-	amp->real_buffer = malloc(sizeof(double) * (FOURIER_SIZE));
-	amp->buffer = malloc(sizeof(float) * BUFFER_SIZE);
-	amp->buffer_index = 0;
-	amp->forward = fftw_plan_dft_r2c_1d(FOURIER_SIZE, amp->real_buffer, amp->complex_buffer, FFTW_ESTIMATE);
-	amp->backward =  fftw_plan_dft_c2r_1d(FOURIER_SIZE, amp->complex_buffer, amp->real_buffer, FFTW_ESTIMATE);
-}
-
-static void fftprocess(Amp* amp, float* buffer)
-{
-	int iterator;
-	double* real_buffer = amp->real_buffer;
-
-	static float ponies = -1.0;
-	for (iterator = 0; iterator < FOURIER_SIZE; iterator++){
-		real_buffer[iterator] = buffer[iterator];
+	for (iterator = 0; iterator < readcount; iterator++) {
+	    buffer[amp->buffer_index + iterator] = input[iterator];
 	}
-	fftw_execute(amp->forward);
-	fftw_execute(amp->backward);
-	for (iterator = 0; iterator < FOURIER_SIZE; iterator++){
-		buffer[iterator] = (float)(real_buffer[iterator] / FOURIER_SIZE);
+	if ((amp->buffer_index < FOURIER_SIZE)
+	    && (amp->buffer_index + readcount >= FOURIER_SIZE)
+	    ) {
+	    // first half of the buffer
+	    fftprocess(amp, amp->buffer);
 	}
+	if (amp->buffer_index + readcount == BUFFER_SIZE) {
+	    // second half of the buffer
+	    // warning: pointer aritmatic
+	    fftprocess(amp, amp->buffer + FOURIER_SIZE);
+	}
+	for (iterator = 0; iterator < readcount; iterator++) {
+	    output[iterator] =
+		buffer[((amp->buffer_index + iterator +
+			 FOURIER_SIZE) % BUFFER_SIZE)];
+	}
+	readindex += readcount;
+	amp->buffer_index = (amp->buffer_index + readcount) % BUFFER_SIZE;
+    } while (readindex < n_samples);
 }
 
-void
-run(LV2_Handle instance, uint32_t n_samples)
-{ 
-	Amp* amp = (Amp*)instance;
-	const float* const input  = amp->input;
-	float* const       output = amp->output;
-	float*      buffer = amp->buffer;
-	uint32_t readindex = 0;
-	uint32_t readcount;
-
-	do {
-		uint32_t iterator;
-		uint32_t bufferlength = (uint32_t)(FOURIER_SIZE - (amp->buffer_index % FOURIER_SIZE));
-		readcount = n_samples;
-		if (amp->buffer_index + readcount > bufferlength) {
-			readcount = bufferlength;
-		}
-
-		for(iterator=0; iterator < readcount; iterator++){
-			buffer[amp->buffer_index + iterator] = input[iterator];
-		}
-		if ((amp->buffer_index < FOURIER_SIZE)
-		&&  (amp->buffer_index + readcount >= FOURIER_SIZE)
-		){
-			// first half of the buffer
-			fftprocess(amp, amp->buffer);
-		}
-		if (amp->buffer_index + readcount == BUFFER_SIZE){
-			// second half of the buffer
-			// warning: pointer aritmatic
-			fftprocess(amp, amp->buffer + FOURIER_SIZE);
-		}
-		for(iterator=0; iterator < readcount; iterator++){
-			output[iterator] = buffer[((amp->buffer_index + iterator + FOURIER_SIZE) % BUFFER_SIZE)];
-		}
-		readindex += readcount;
-		amp->buffer_index = (amp->buffer_index + readcount) % BUFFER_SIZE;
-	} while(readindex < n_samples);
-}
-
-static void
-deactivate(LV2_Handle instance)
+static void deactivate(LV2_Handle instance)
 {
 }
 
-static void
-cleanup(LV2_Handle instance)
+static void cleanup(LV2_Handle instance)
 {
-	free(instance);
+    free(instance);
 }
 
-const void*
-extension_data(const char* uri)
+const void *extension_data(const char *uri)
 {
-	return NULL;
+    return NULL;
 }
 
 static const LV2_Descriptor descriptor = {
-	AMP_URI,
-	instantiate,
-	connect_port,
-	activate,
-	run,
-	deactivate,
-	cleanup,
-	extension_data
+    AMP_URI,
+    instantiate,
+    connect_port,
+    activate,
+    run,
+    deactivate,
+    cleanup,
+    extension_data
 };
 
-LV2_SYMBOL_EXPORT
-const LV2_Descriptor*
-lv2_descriptor(uint32_t index)
+LV2_SYMBOL_EXPORT const LV2_Descriptor *lv2_descriptor(uint32_t index)
 {
-	switch (index) {
-	case 0:
-		return &descriptor;
-	default:
-		return NULL;
-	}
+    switch (index) {
+    case 0:
+	return &descriptor;
+    default:
+	return NULL;
+    }
 }

317b674ea9b2bed2c699a2dea9e509612b35733a
Nido Media <nido@foxserver.be>
2013-03-31 19:12:06 +0200

    changed buffer structure added pegfile support (=requirement)
    fixed error that it did not work properly if the n_samples is larger then the fourier size

--- nidoamp/nidoamp.c
+++ nidoamp/nidoamp.c
@@ -20,13 +20,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdbool.h>
 #include <complex.h>
 #include <fftw3.h>
 
 //#include "lv2/lv2plug.in/ns/lv2core/lv2.h"
 #include <lv2.h>
 
-#define AMP_URI "http://nidomedia.com/lv2/nidoamp"
+#include "nidoamp.peg"
+
 
 #ifndef FOURIER_SIZE
 #define FOURIER_SIZE 512
@@ -36,24 +38,14 @@
 #define COMPLEX_SIZE (FOURIER_SIZE / 2 + 1)
 #endif
 
-
-
-#define BUFFER_SIZE (FOURIER_SIZE * 2)
-
-
-typedef enum {
-	AMP_HIPASS   = 0,
-	AMP_INPUT  = 1,
-	AMP_OUTPUT = 2
-} PortIndex;
-
 typedef struct {
 	float* hipass;
 	float* input;
 	float* output;
 	fftw_complex* complex_buffer;
 	double* real_buffer;
-	float* buffer;
+	float* inbuffer;
+	float* outbuffer;
 	int buffer_index;
 	fftw_plan forward;
 	fftw_plan backward;
@@ -68,10 +60,13 @@ instantiate(const LV2_Descriptor * descriptor,
 	amp->complex_buffer = fftw_malloc(sizeof(fftw_complex) * COMPLEX_SIZE);
 	amp->real_buffer = fftw_malloc(sizeof(double) * (FOURIER_SIZE));
 	// todo: malloc when latency is implemented.
-	amp->buffer = calloc(sizeof(float), BUFFER_SIZE);
+	amp->inbuffer = calloc(sizeof(float), FOURIER_SIZE);
+	amp->outbuffer = calloc(sizeof(float), FOURIER_SIZE);
 	amp->buffer_index = 0;
 	amp->forward = fftw_plan_dft_r2c_1d(FOURIER_SIZE, amp->real_buffer, amp->complex_buffer, FFTW_ESTIMATE);
 	amp->backward =  fftw_plan_dft_c2r_1d(FOURIER_SIZE, amp->complex_buffer, amp->real_buffer, FFTW_ESTIMATE);
+	// making this zero (float zero's are zero too)
+	amp->real_buffer = memset(amp->real_buffer, 0, sizeof(double) * FOURIER_SIZE);
 
     return (LV2_Handle) amp;
 }
@@ -80,14 +75,14 @@ static void connect_port(LV2_Handle instance, uint32_t port, void *data)
 {
 	Amp* amp = (Amp*)instance;
 
-	switch ((PortIndex)port) {
-	case AMP_HIPASS:
+	switch ((enum nidoamp_port_enum)port) {
+	case nidoamp_hipass:
 		amp->hipass = (float*)data;
 		break;
-	case AMP_INPUT:
+	case nidoamp_in:
 		amp->input = (float*)data;
 		break;
-	case AMP_OUTPUT:
+	case nidoamp_out:
 		amp->output = (float*)data;
 		break;
 	}
@@ -98,7 +93,7 @@ void activate(LV2_Handle instance)
 	Amp* amp = (Amp*)instance;
 }
 
-static void fftprocess(Amp * amp, float *buffer)
+static void fftprocess(Amp * amp)
 {
 	int iterator;
 	double* real_buffer = amp->real_buffer;
@@ -106,17 +101,17 @@ static void fftprocess(Amp * amp, float *buffer)
 	int hipass = (int) *(amp->hipass);
 
 	for (iterator = 0; iterator < FOURIER_SIZE; iterator++){
-		real_buffer[iterator] = buffer[iterator];
+		real_buffer[iterator] = amp->inbuffer[iterator];
 	}
 	fftw_execute(amp->forward);
 	for (iterator = 0; iterator < COMPLEX_SIZE; iterator++){
-		if (COMPLEX_SIZE - iterator > hipass) {
-			complex_buffer[iterator] = 0.0 * I;
+		if (iterator < hipass) {
+			complex_buffer[iterator] *= 0.0;
 		}
 	}
 	fftw_execute(amp->backward);
 	for (iterator = 0; iterator < FOURIER_SIZE; iterator++){
-		buffer[iterator] = (float)(real_buffer[iterator] / FOURIER_SIZE);
+		amp->outbuffer[iterator] = (float)(real_buffer[iterator] / FOURIER_SIZE);
 	}
 }
 
@@ -126,38 +121,29 @@ run(LV2_Handle instance, uint32_t n_samples)
         Amp* amp = (Amp*)instance;
         const float* const input  = amp->input;
         float* const       output = amp->output;
-        float*      buffer = amp->buffer;
+        float*      inbuffer = amp->inbuffer;
+        float*      outbuffer = amp->outbuffer;
         uint32_t readindex = 0;
         uint32_t readcount;
 
         do {
                 uint32_t iterator;
-                uint32_t bufferlength = (uint32_t)(FOURIER_SIZE - (amp->buffer_index % FOURIER_SIZE));
+                uint32_t bufferlength = (uint32_t)(FOURIER_SIZE - amp->buffer_index);
                 readcount = n_samples;
                 if (amp->buffer_index + readcount > bufferlength) {
                         readcount = bufferlength;
                 }
 
                 for(iterator=0; iterator < readcount; iterator++){
-                        buffer[amp->buffer_index + iterator] = input[iterator];
-                }
-                if ((amp->buffer_index < FOURIER_SIZE)
-                &&  (amp->buffer_index + readcount >= FOURIER_SIZE)
-                ){
-                        // first half of the buffer
-                        fftprocess(amp, amp->buffer);
-                }
-                if (amp->buffer_index + readcount == BUFFER_SIZE){
-                        // second half of the buffer
-                        // warning: pointer aritmatic
-                        fftprocess(amp, amp->buffer + FOURIER_SIZE);
-                }
-                for(iterator=0; iterator < readcount; iterator++){
-                        output[iterator] = buffer[((amp->buffer_index + iterator + FOURIER_SIZE) % BUFFER_SIZE)];
+                        inbuffer[amp->buffer_index + iterator] = input[readindex + iterator];
+			output[readindex + iterator] = outbuffer[amp->buffer_index + iterator];
                 }
 
+                if (amp->buffer_index + readcount == FOURIER_SIZE){
+                        fftprocess(amp);
+                }
 	readindex += readcount;
-	amp->buffer_index = (amp->buffer_index + readcount) % BUFFER_SIZE;
+	amp->buffer_index = (amp->buffer_index + readcount) % FOURIER_SIZE;
     } while (readindex < n_samples);
 }
 
@@ -176,7 +162,7 @@ const void *extension_data(const char *uri)
 }
 
 static const LV2_Descriptor descriptor = {
-    AMP_URI,
+    nidoamp_uri,
     instantiate,
     connect_port,
     activate,

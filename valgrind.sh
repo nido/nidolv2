1ad4eb7a9352c3a4b7b39cba7be6c8307b21e814
Nido Media <nido@foxserver.be>
2013-04-02 13:39:44 +0200

    valgrind.sh now alternatively starts gdb when not successful in running the program

--- valgrind.sh
+++ valgrind.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make check && /usr/bin/valgrind -v --leak-check=full --show-reachable=yes nidoamp/test_nidoamp
+make check && /usr/bin/valgrind -v --leak-check=full --show-reachable=yes nidoamp/test_nidoamp || gdb nidoamp/test_nidoamp

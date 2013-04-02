bf1d12247aa4e592a5890582e16efc17525c3f71
Nido Media <nido@foxserver.be>
2013-04-02 16:12:46 +0200

    sorta work with the latency in the test program

--- nidoamp/test_nidoamp.c
+++ nidoamp/test_nidoamp.c
@@ -13,21 +13,27 @@ int main(void)
     LV2_Handle handle;
     int i;
     int j;
+    float latency;
 
     j = 0;
     hipass = 1.0;
-    buffer = malloc(sizeof(float) * DATASIZE);
+
+    handle = instantiate(&descriptor, 96000.0, "Irrelevant", NULL);
+    connect_port(handle, 4, &latency);
+    buffer = malloc(sizeof(float) * (DATASIZE + (int)latency));
+    for (i = 0; i < DATASIZE; i++) {
+	buffer[i] = (float) rand() / RAND_MAX * 2 - 1;
+    }
+    connect_port(handle, 0, &hipass);
+    connect_port(handle, 1, buffer);
+    connect_port(handle, 2, buffer);
+
     if (buffer == NULL) {
 	printf("%s\n", "Not enough memory");
 	exit(EXIT_FAILURE);
     }
-    for (i = 0; i < DATASIZE; i++) {
-	buffer[i] = (float) rand() / RAND_MAX * 2 - 1;
-    }
 
-    handle = instantiate(&descriptor, 96000.0, "Irrelevant", NULL);
-    connect_port(handle, 0, &hipass);
-    connect_port(handle, 1, buffer);
+    run(handle, (int)latency);
     connect_port(handle, 2, buffer);
     activate(handle);
     for (i = 0; i < DATASIZE; i += j) {

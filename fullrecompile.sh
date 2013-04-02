45d3d7e6ccf3620020158bf1a6fef7a43e8a7517
Nido Media <nido@foxserver.be>
2013-04-02 16:06:45 +0200

    ${home}->${HOME} in fullrecomplie

--- fullrecompile.sh
+++ fullrecompile.sh
@@ -2,4 +2,4 @@
 # full cleanup if possible
 test -f Makefile && make maintainer-clean
 
-autoreconf -si && ./configure --with-fouriersize=512 --bindir=${home}/bin --with-lv2dir=${HOME}/.lv2 && make check
+autoreconf -si && ./configure --with-fouriersize=512 --bindir=${HOME}/bin --with-lv2dir=${HOME}/.lv2 && make check

438bd141d31a9049470f5f135f57e20aa36f6576
Nido Media <nido@toad.foxserver.be>
2013-04-10 00:07:55 +0200

    removed splint blaat to accomodate indenting

--- nidoamp/nidoamp.h
+++ nidoamp/nidoamp.h
@@ -24,12 +24,11 @@ typedef struct {
 const LV2_Descriptor descriptor;
 
 LV2_Handle instantiate(const LV2_Descriptor * descriptor, double rate,
-		       const char *bundle_path,
-		       /*@null@ */ const LV2_Feature * const *features);
+                       const char *bundle_path,
+                       /*@null@ */ const LV2_Feature * const *features);
 void connect_port(LV2_Handle instance, uint32_t port, void *data);
 void activate(LV2_Handle instance);
 void run(LV2_Handle instance, uint32_t n_samples);
 void deactivate(LV2_Handle instance);
 void cleanup( /*@only@ */ LV2_Handle instance);
-		     /*@null@ *//*@shared@ */ LV2_SYMBOL_EXPORT const LV2_Descriptor
-* lv2_descriptor(uint32_t index);
+LV2_SYMBOL_EXPORT const LV2_Descriptor *lv2_descriptor(uint32_t index);

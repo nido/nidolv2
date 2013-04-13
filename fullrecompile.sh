99d969d3c48ae93d06dbe3c628f2748a1abc6d8b
Nido Media <nido@foxserver.be>
2013-04-13 19:01:49 +0200

    added link to fullrecompile to output documentation to ~/Documentation

--- fullrecompile.sh
+++ fullrecompile.sh
@@ -2,4 +2,4 @@
 # full cleanup if possible
 test -f Makefile && make maintainer-clean
 
-autoreconf -fsi && ./configure --with-fouriersize=512 --bindir=${HOME}/bin --with-lv2dir=${HOME}/.lv2 && make check
+autoreconf -fsi && ./configure --with-fouriersize=512 --bindir=${HOME}/bin --docdir=${HOME}/Documents --with-lv2dir=${HOME}/.lv2 && make check distcheck

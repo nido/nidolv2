8c7b9b4481a520bdb8ce83333b9b2b57e1968e7c
Nido Media <nido@foxserver.be>
2013-04-15 01:11:15 +0200

    moved test.c to test_ringbuffer and made it part of make check

---
+++ nidoamp/test_ringbuffer.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include "nidoamp/ringbuffer.h"
+
+void main(void){
+float* buffer;
+
+struct ringbuffer* yay;
+int i;
+
+buffer = malloc(sizeof(float) * 1024);
+yay = init_buffer(1024, 0);
+
+for (i=0; i<512; i++){
+	read_buffer(buffer, yay, 1020);
+	write_buffer(yay, buffer, 1020);
+}
+
+buffer = malloc(sizeof(float) * 1024);
+yay = init_buffer(1024, -1);
+
+for (i=0; i<512; i++){
+	read_buffer(buffer, yay, 1020);
+	write_buffer(yay, buffer, 1020);
+}
+
+}

c95ce8a765026fa6e936a94d14eb8eb22f210c27
Nido Media <nido@foxserver.be>
2013-09-09 01:12:02 +0200

    added sse version of the averaging algorithm

--- nidoamp/nidoamp.c
+++ nidoamp/nidoamp.c
@@ -2,6 +2,10 @@
  * spectral filter, as specified by LV2 on http://lv2plug.in/
  */
 
+#ifdef __SSE3__
+#include <pmmintrin.h>
+#endif                          //__SSE3__
+
 #include <assert.h>
 #include <math.h>
 #include <stdlib.h>
@@ -11,6 +15,7 @@
 
 #include "nidoamp.h"
 #include "nidoamp.peg"
+#include "cpuid.h"
 
 #include "innerproduct.h"
 
@@ -225,6 +230,40 @@ void compute_kernel(Amp * amp)
     //printf("normalisation factor %f\n", normalisation_factor);
 }
 
+#ifdef __SSE__
+/** returns a kernel averaged from the two kernels between them, sse
+ * version
+ *
+ * @param kernel the return pointer for the kernel
+ * @param amp the amp which holds the kernels
+ */
+void average_kernels_sse(float *kernel, Amp * amp)
+{
+    int i;
+	__m128 right;
+	__m128 left;
+	__m128 out;
+	__m128 leftindex = {1.0f, 2.0f, 3.0f, 4.0f};
+	__m128 rightindex = { FOURIER_SIZE - 1.0, FOURIER_SIZE - 2.0, FOURIER_SIZE - 3.0, FOURIER_SIZE - 4.0};
+	__m128 fouriersize = _mm_set_ps1((float)FOURIER_SIZE);
+	__m128 jumpsize = _mm_set_ps1(4.0f);
+    for (i = 0; i < FOURIER_SIZE; i=i+4) {
+		left = _mm_loadu_ps(amp->previous_buffer + i);
+		right = _mm_loadu_ps(amp->fourier_buffer + i);
+		out = _mm_div_ps( _mm_add_ps( _mm_mul_ps(left, leftindex), _mm_mul_ps(right, rightindex)), fouriersize);
+		leftindex = _mm_add_ps(leftindex, jumpsize);
+		rightindex = _mm_sub_ps(rightindex, jumpsize);
+		_mm_store_ps(amp->fourier_buffer+i, out);
+	}
+	for (; i < FOURIER_SIZE; i++) {
+        kernel[i] =
+            (amp->previous_buffer[i] * (i + 1) +
+             amp->fourier_buffer[i] * (FOURIER_SIZE - i -
+                                       1)) / (FOURIER_SIZE);
+    }
+}
+#endif //__SSE__
+
 /** returns a kernel averaged from the two kernels between them
  *
  * @param kernel the return pointer for the kernel
@@ -278,7 +317,16 @@ void fftprocess(Amp * amp)
 
         prefetch_buffer(inbuf, amp->in_buffer, FOURIER_SIZE, i);
 
+#ifdef __SSE__
+		if (has_sse()){
+	        average_kernels_sse(kernel, amp);
+		} else {
+        	average_kernels(kernel, amp);
+			exit(EXIT_FAILURE);
+		}
+#else
         average_kernels(kernel, amp);
+#endif
         //bcopy(kernel, amp->fourier_buffer, sizeof(float) * FOURIER_SIZE);
 
         output[i] = (amp->convolve_func) (inbuf, kernel);
